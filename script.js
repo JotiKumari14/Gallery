633844e0447ea9bd7088cc66f1ea56cc029d3e60
Joti Kumari <jotiwaduwani22@gmail.com>
2026-07-18 01:04:15 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var expo = document.querySelector("button")
+expo.addEventListener("click",function(){
+    expo.style.backgroundColor = "#3faefd"
+    expo.style.color = "#010101"
+
+})
+var back = document.querySelector(".main #back")
+back.addEventListener("click",function(){
+    back.style.backgroundColor = "#3faefd"
+    back.style.color = "#010101"
+
+})
+
